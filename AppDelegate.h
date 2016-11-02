991f942582cf8a957539a22ea508828eac30277b
760766307 <jiangedabaojian@qq.com>
2016-11-02 18:55:36 +0800

    第一版本
    这就是第一版本咯

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xinkaopay
+//
+//  Created by 杨剑 on 16/11/2.
+//  Copyright © 2016年 贱贱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xinkaopay
+//
+//  Created by 杨剑 on 16/11/2.
+//  Copyright © 2016年 贱贱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
